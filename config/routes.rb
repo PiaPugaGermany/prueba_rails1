622bc7070095c84655539f0f68934ba59b7013f0
piapugagermany <pipuge@gmail.com>
2019-07-13 13:44:11 -0400

    Prueba de rails 1

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'todos', to: 'todos#index'
+  get 'todos/new'
+  post 'todos', to: 'todos#create'
+  get 'todos/:id', to: 'todos#show', as: :todo
+  get 'todos/:id/edit', to: 'todos#edit', as: :edit_todo
+  patch 'todos/:id', to: 'todos#update'
+  delete 'todos/:id', to: 'todos#destroy'
+  get 'todos/:id/complete', to: 'todos#complete', as: :complete_todo
+  get 'list', to: 'todos#list', as: :list_todo
+end
